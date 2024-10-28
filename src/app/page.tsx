98c6d688b52197d9c4b38ca452e4d5d003bfa8e9
Sam Selikoff <sam.selikoff@gmail.com>
2024-10-28 14:55:37 -0400

    Initial demos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,85 +2,73 @@
 
 import { generateText } from "@/app/actions";
 import { useState } from "react";
-import Together from "together-ai";
 import { ChatCompletionStream } from "together-ai/lib/ChatCompletionStream";
 
 export default function Chat() {
-  const [messages, setMessages] = useState<
-    Together.Chat.Completions.CompletionCreateParams.Message[]
-  >([]);
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [isPending, setIsPending] = useState(false);
 
   async function handleAction(formData: FormData) {
     const content = formData.get("prompt");
     if (typeof content !== "string") return;
+    setIsPending(true);
+    setQuestion(content);
 
-    setMessages((messages) => [
-      ...messages,
-      {
-        role: "user",
-        content,
-      },
-    ]);
-
-    const stream = await generateText([
-      ...messages,
-      {
-        role: "user",
-        content,
-      },
-    ]);
+    const stream = await generateText([{ role: "user", content }]);
     const runner = ChatCompletionStream.fromReadableStream(stream);
-    runner.on("content", (delta) => {
-      setMessages((messages) => {
-        const lastMessage = messages.at(-1);
-
-        if (lastMessage?.role !== "assistant") {
-          return [...messages, { role: "assistant", content: delta }];
-        } else {
-          return [
-            ...messages.slice(0, -1),
-            { ...lastMessage, content: lastMessage.content + delta },
-          ];
-        }
-      });
-    });
+    runner
+      .on("content", (delta) => setAnswer((text) => text + delta))
+      .on("end", () => setIsPending(false));
   }
 
   return (
-    <div className="mx-auto w-full max-w-5xl">
-      <div className="space-y-4 p-8">
-        {messages.map((message, i) => (
-          <div key={i} className="flex">
-            {message.role === "user" ? (
-              <div className="ml-auto rounded-full bg-gray-200 px-4 py-2">
-                {message.content}
-              </div>
-            ) : (
-              <div>{message.content}</div>
-            )}
-          </div>
-        ))}
-      </div>
+    <div className="mx-auto flex w-full max-w-3xl grow flex-col px-4">
+      {!question ? (
+        <div className="flex grow flex-col justify-center">
+          <form action={handleAction} className="flex w-full">
+            <fieldset className="flex w-full gap-2" disabled={isPending}>
+              <input
+                placeholder="Ask anything..."
+                autoFocus
+                name="prompt"
+                className="block w-full rounded border border-gray-300 p-2"
+              />
+              <button
+                className="rounded bg-black px-3 py-1 font-medium text-white"
+                type="submit"
+              >
+                Submit
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      ) : (
+        <>
+          <div className="mt-8 flex flex-col justify-end">
+            <div className="grid grid-cols-4">
+              <p className="col-span-3 text-xl">{question}</p>
 
-      <div className="fixed inset-x-0 bottom-0 m-8 flex justify-center gap-2">
-        <form
-          action={handleAction}
-          className="flex w-full max-w-5xl items-center gap-2"
-        >
-          <textarea
-            placeholder="Ask anything..."
-            name="prompt"
-            rows={1}
-            className="block w-full rounded border border-gray-300 p-2"
-          />
-          <button
-            className="rounded bg-black px-3 py-1 font-medium text-white"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
+              <div className="text-right">
+                <button
+                  className="rounded bg-black px-3 py-2 font-medium text-white disabled:opacity-50"
+                  disabled={isPending}
+                  onClick={() => {
+                    setQuestion("");
+                    setAnswer("");
+                  }}
+                >
+                  Reset
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="py-8">
+            <p className="whitespace-pre-wrap">{answer}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
